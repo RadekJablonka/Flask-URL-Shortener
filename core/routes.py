9e4b14c4b3da2c43f02aa008dc0c0615bde2244c
Jablonka <radoslaw.jablonka@capgemini.com>
2022-03-27 01:18:24 +0100

    adjusting the project

--- core/routes.py
+++ core/routes.py
@@ -3,59 +3,70 @@ from core.models import ShortUrls
 from core import app, db
 from random import choice
 import string
+import validators
 from flask import render_template, request, flash, redirect, url_for
 
 
 def generate_short_id(num_of_chars: int):
     """Function to generate short_id of specified number of characters"""
-    return ''.join(choice(string.ascii_letters+string.digits) for _ in range(num_of_chars))
+    return "".join(
+        choice(string.ascii_letters + string.digits) for _ in range(num_of_chars)
+    )
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == 'POST':
-        url = request.form['url']
-        short_id = request.form['custom_id']
-        expiration_date = request.form['expiration_date']
+    if request.method == "POST":
+        url = request.form["url"].replace(" ", "")
+        short_id = request.form["custom_id"]
+        expiration_date = request.form["expiration_date"]
 
-        if short_id and ShortUrls.query.filter_by(short_id=short_id).first() is not None:
-            flash('Please enter different custom id!')
-            return redirect(url_for('index'))
+        if (
+            short_id
+            and ShortUrls.query.filter_by(short_id=short_id).first() is not None
+        ):
+            flash("Please enter different Custom Short ID!")
 
-        if not url:
-            flash('The URL is required!')
-            return redirect(url_for('index'))
+            return redirect(url_for("index"))
 
-        if len(expiration_date)>0:
+        if not url or not bool(validators.url(url)):
+            flash("The valid URL is required!")
+            return redirect(url_for("index"))
+
+        if len(expiration_date) > 0:
             try:
                 int(expiration_date)
             except ValueError:
-                flash('Please enter the expiration time in minutes')
-                return redirect(url_for('index'))
+                flash("Please enter the expiration time in minutes")
+                return redirect(url_for("index"))
 
         if not short_id:
             short_id = generate_short_id(8)
 
         expiration_in_minutes = int(expiration_date) if expiration_date else 15
         new_link = ShortUrls(
-            original_url=url, short_id=short_id, created_at=datetime.now(), expiration_date=datetime.now() + timedelta(minutes=expiration_in_minutes))
+            original_url=url,
+            short_id=short_id,
+            created_at=datetime.now(),
+            expiration_date=datetime.now() + timedelta(minutes=expiration_in_minutes),
+        )
         db.session.add(new_link)
         db.session.commit()
         short_url = request.host_url + short_id
 
-        return render_template('index.html', short_url=short_url)
+        return render_template("index.html", short_url=short_url)
 
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@app.route('/<short_id>')
+@app.route("/<short_id>")
 def redirect_url(short_id):
     link = ShortUrls.query.filter_by(short_id=short_id).first()
     if link and link.expiration_date < datetime.now():
-        flash(f'Custom short id {short_id} has expired')
-        return redirect(url_for('index'))
+        flash(f"Custom short id {short_id} has expired")
+        return redirect(url_for("index"))
     elif link:
         return redirect(link.original_url)
     else:
-        flash('Invalid URL')
-        return redirect(url_for('index'))
+        flash("Invalid URL")
+        return redirect(url_for("index"))
